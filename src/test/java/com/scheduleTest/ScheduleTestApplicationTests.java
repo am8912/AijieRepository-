8bbfe9a4ed06ee4ab89fea0672775b2c3c843c27
am8912 <103872283+am8912@users.noreply.github.com>
2023-09-16 18:44:57 +0800

    0916 spring boot

---
+++ src/test/java/com/scheduleTest/ScheduleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
